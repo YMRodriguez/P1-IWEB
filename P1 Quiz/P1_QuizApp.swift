3c9908e701dab1189a81a2b31344615e9d243101
Yamil Mateo Rodríguez <yamilmateor@gmail.com>
2020-10-10 15:42:47 +0200

    Initial Commit

---
+++ P1 Quiz/P1_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  P1_QuizApp.swift
+//  P1 Quiz
+//
+//  Created by Yamil Mateo Rodriguez on 10/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct P1_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
